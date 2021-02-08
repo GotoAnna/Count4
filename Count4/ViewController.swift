89b9fbd54c58d4a48063316ba1177183ee320712
AnnaGoto <tomato0141pasta@gmail.com>
2021-02-08 14:14:36 +0900

    count4完成

--- Count4/ViewController.swift
+++ Count4/ViewController.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func plus()
+    {
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus()
+    {
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func clear()
+    {
+        number = 0
+        label.text = String(number)
+    }
+    
 
 
 }
